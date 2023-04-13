5828a82c608da83c04329d48639040ec28b24030
GuillaumeBiege <Guillaume.biege@gmail.com>
2023-04-13 20:41:19 +0200

    First test on gameplay abilities

--- Source/ElementalBirthUE5/ElementalBirthUE5.Build.cs
+++ Source/ElementalBirthUE5/ElementalBirthUE5.Build.cs
@@ -8,7 +8,7 @@ public class ElementalBirthUE5 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "Niagara" });
+		PublicDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks", "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "Niagara" });
 		//PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Niagara"));
 	}
 }
